89c0712050b76a50e12f5b7a9ccba4ce69fde9bd
percy <percy01729@gmail.com>
2025-09-11 03:18:07 +0530

    some env

--- appbackend/server.js
+++ appbackend/server.js
@@ -15,7 +15,9 @@ app.use('/api/reports',reportRoutes);
 app.get("/ping", (req, res) => {
   res.json({ message: "Server is alive 🚀" });
 });
-const PORT=3000;
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on http://0.0.0.0:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
